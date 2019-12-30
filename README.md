62c529f97035732f8a82a3710fd25d4f3db753df
Felipe@MBP2 <auditor400@gmail.com>
2019-12-30 16:15:10 +0530

    add docs, example config

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# running:
+
+install go 1.3 or superior
+
+# configuring
+
+create an empty private folder and put a file that looks like private_sample
+
+
+
---
+++ private_sample/config.js
@@ -0,0 +1,8 @@
+/**
+ This is an otto (Javascript) config file example, feel free to change to your needs.
+ */
+
+function getRowColor(row, dbname,fieldName){
+  return "";
+}
+
